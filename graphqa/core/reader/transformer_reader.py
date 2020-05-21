29447b617bc8ee83d1b16f0e3e35b7f82b1305e8
Aleksandar Varga <aleksandar.varga@uns.ac.rs>
2020-05-21 16:52:30 +0200

    introducing transformer based nn reader

---
+++ graphqa/core/reader/transformer_reader.py
@@ -0,0 +1,66 @@
+import torch
+from simpletransformers.question_answering import QuestionAnsweringModel
+
+from .abstract_reader import AbstractReader
+
+
+class TransformerReader(AbstractReader):
+
+    def __init__(self):
+        super().__init__()
+        self.model = None
+
+    def load(self, path):
+        use_cuda = torch.cuda.is_available()
+
+        if not use_cuda:
+            raise UserWarning('CUDA not available.')
+
+        try:
+            import apex
+            fp16 = True
+        except ImportError:
+            fp16 = False
+
+        self.model = QuestionAnsweringModel(
+            model_type='albert',
+            model_name=path,
+            use_cuda=use_cuda,
+            args={'fp16': fp16}
+        )
+
+    def extract_answer(self, question, paragraphs):
+        if not paragraphs:
+            return [{
+                'answer': '',
+                'context': '',
+                'confidence': 0,
+                'supportingFacts': []
+            }]
+
+        to_predict = []
+        contexts = {}
+        for idx, entry in enumerate(paragraphs):
+            context = self._get_context(entry)
+            contexts[idx] = context
+            to_predict.append({
+                'context': context,
+                'qas': [{'question': question, 'id': idx}]
+            })
+
+        predictions = self.model.predict(to_predict)
+        results = []
+        for prediction in predictions:
+            idx = prediction['id']
+            context = contexts[idx]
+            answer = prediction['answer']
+            results.append({
+                'answer': answer,
+                'confidence': 1.0 if answer else 0.5,
+                'context': context,
+                'supporting_facts': [],
+            })
+        return results
+
+    def _get_context(self, paragraphs):
+        return ' '.join(map(lambda p: p['text'], paragraphs))
